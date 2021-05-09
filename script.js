6a3f0b8a5b70f55890878cdde4aac0542830bdd0
Asher <asher.bergtraun@gmail.com>
2021-05-08 21:36:31 -0700

    index connect to javascript

--- script.js
+++ script.js
@@ -8,7 +8,21 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var text;
+  var favDrink = prompt("What's your favorite cocktail drink?", "Daiquiri");
+  switch(favDrink) {
+    case "Martini":
+      text = "Excellent choice. Martini is good for your soul.";
+      break;
+    case "Daiquiri":
+      text = "Daiquiri is my favorite too!";
+      break;
+    case "Cosmopolitan":
+      text = "Really? Are you sure the Cosmopolitan is your favorite?";
+      break;
+    default:
+      text = "I have never heard of that one..";
+  }
   passwordText.value = password;
 
 }
