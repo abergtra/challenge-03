a17bf56710a030836c112e15ecab69c656b213ac
Asher <asher.bergtraun@gmail.com>
2021-05-09 10:47:28 -0700

    framework for password array

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var LowerCase = "abcdefghijklmnopqrstuvwxyz";
 var Numeric = "0123456789";
 var SpecialCharacter = "!@#$%^&*()_+=[]";
 var leng = 0;
+var content = 0;
 
 // Functions to randomly select characters
 function getLowerCase() {
@@ -33,16 +34,64 @@ function passwordLength() {
       alert ("Please choose a password length between 8 and 128!");
     } else {
       return leng;
+      var content = passwordContent();
     }
-  
-  
 }
 
+//generate the password
 function generatePassword() {
   var leng = passwordLength();
-  alert(leng);
+  alert("you chose password length:" + leng);
+  let pass = "";
+
+  //create password array based on length
+  for (let i = 0; i < leng; i++) {
+    const x = generateX();
+    pass += x;
+  }
+
+  passwordText.innerText = pass;
 }
 
+//confirm type of content in password
+function passwordContent() {
+  var cuppercase = confirm("Would you like to use uppercase letters?");
+  var clowercase = confirm("Would you like to use lowercase letters?");
+  var cnumbers = confirm("would you like to use numbers?");
+  var csymbols = confirm("would you like to use special characters?");
+
+  while (!(cuppercase || clowercase || cnumbers || csymbols)) {
+    alert("You must select at least one character type!");
+
+    cuppercase = confirm("Would you like to use uppercase letters?");
+    clowercase = confirm("Would you like to use lowercase letters?");
+    cnumbers = confirm("would you like to use numbers?");
+    csymbols = confirm("would you like to use special characters?");
+  }
+}
+
+
+//assign random values to password array
+function generateX() {
+  const xarr = [];
+
+  xarr.push(getUpperCase());
+  
+  xarr.push(getLowerCase());
+  
+  xarr.push(getNumerical());
+  
+  xarr.push(getSpecialCharacter());
+
+  if (xarr.length > 0) {
+    return xs[Math.floor(Math.random() * xs.length)];
+  } else {
+    alert ("You must select at leaset 1 of the following: Upper Case Letter, Lower Case Letter, Numerical, Special Character.")
+  }
+    
+
+  
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
